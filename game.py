3dd00ab3a9ae458f3246e89354bf21defcb62433
Gurmukh-Singh-4253 <gurmukh050304@gmail.com>
2025-02-27 10:50:09 +0530

    fixed rotation, added flight

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import pygame
-import neat
 import time
 import os
 import random
+import math
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
@@ -26,35 +26,29 @@ class Bird:
         self.y = y
         self.tilt = 0
         self.tick_count = 0
-        self.vel = 0
+        self.y_vel = 0
+        self.x_vel = 15
         self.height = y
         self.img_count = 0
         self.img = self.IMGS[0]
     
     def jump(self):
-        self.vel = -10.5
+        self.y_vel = -32
         self.tick_count = 0
         self.height = self.y
 
     def move(self):
         self.tick_count += 1
         
-        d = self.vel * self.tick_count + 1.5 * self.tick_count ** 2
+        self.y_vel += 5
 
-        if d >= 16:
-            d = 16
+        if self.y_vel >= 20:
+            self.y_vel = 20
         
-        if d < 0:
-            d -= 2
+        if self.y_vel < -32:
+            self.y_vel = -52
         
-        self.y = self.y + d
-
-        if d < 0 or self.y < self.height + 50:
-            if self.tilt < self.MAX_ROTATION:
-                self.tilt = self.MAX_ROTATION
-        else:
-            if self.tilt > -90:
-                self.tilt -= self.ROT_VEL
+        self.y = self.y + self.y_vel
 
     def draw(self, win):
         self.img_count += 1
@@ -71,21 +65,22 @@ class Bird:
             self.img = self.IMGS[0]
             self.img_count = 0
 
-        if self.tilt <= -80:
-            self.img = self.IMGS[1]
-            self.img_count = self.ANIMATION_TIME * 2
+        self.tilt = - math.atan(self.y_vel/self.x_vel)
+        print(self.y_vel, self.x_vel, self.tilt)
         
-        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        rotated_image = pygame.transform.rotate(self.img, self.tilt * 180 / 3.1416)
         new_rect = rotated_image.get_rect(center = self.img.get_rect(topleft = (self.x, self.y)).center)
         win.blit(rotated_image, new_rect.topleft)
     
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
+
 def draw_window(win, bird):
     win.blit(BG_IMG, (0, 0))
     bird.draw(win)
     pygame.display.update()
+
 
 def main():
     bird = Bird(200, 200)
@@ -98,6 +93,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                    bird.jump()
 
         bird.move()
         draw_window(win, bird)
